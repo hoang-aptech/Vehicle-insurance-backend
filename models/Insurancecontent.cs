dba27a5f06e1b495c6b32017ef0a5b26a2210fe7
Hoang0504 <ngochoanghuy0504@gmail.com>
2024-10-05 09:50:48 +0700

    add insurance-content class

---
+++ models/Insurancecontent.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace vehicle_insurance_backend.models
+{
+    public class Insurancecontent
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public int InsuranceId { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 5)]
+        public string ContentType { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 10)]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        
+    }
+}
